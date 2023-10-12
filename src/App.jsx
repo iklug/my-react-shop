af4565b9e9bde6743373363681bec84c9bff7ef9
Ian Klug <ianklug123@gmail.com>
2023-10-12 16:55:33 -0700

    fixed header, body padding

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Card from './components/Card'
 import bookObject from './data/bookObject'
-
+import Header from './components/Header'
 function App() {
 
 
   return (
-    <div className="w-4/5 m-auto">
-    {bookObject.map(item => <Card book={item}/>)}
-    </div>
+    <div className="">
+      <Header />
+      <div className='pt-32'>
+        {bookObject.map(item => <Card book={item} key={item.isbn}/>)}
+      </div>
+      </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 
 const Header = () => {
     return (
-        <></>
+     
+        <div className="flex flex-row bg-stone-50 shadow-md mb-12 fixed w-full">
+            <div className="h-24  flex">
+                <div>ICON</div>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>Browse</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="flex">
+            <ul>
+                <li>Wishlist</li>
+                <li>Cart</li>
+            </ul>
+            </div>
+            
+        </div>
     )
 }
 
