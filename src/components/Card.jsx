8773f8c8f0ac5ed0e7686adea3da4dbcf9faf553
Ian Klug <ianklug123@gmail.com>
2023-10-12 14:43:29 -0700

    create card-component branch

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
 
 const Card = () => {
     return (
-        <></>
+        <div className="h-60 border-2 border-slate-100">
+            <div></div>
+            <div></div>
+        </div>
     )
 }
 
