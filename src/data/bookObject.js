0502769f75ee1ea886890ce762b92595085354ee
Ian Klug <ianklug123@gmail.com>
2023-10-12 12:54:34 -0700

    finish bookObject

---
+++ src/data/bookObject.js
@@ -0,0 +1,61 @@
+
+export default const bookObject = [
+     {
+        title: 'The Way of Kings',
+        length: 1007,
+        isbn: '9780765326355',
+        author: 'Brandon Sanderson',
+        series: 'The Stormlight Archive',
+    },
+    {
+        title: 'Words of Radiance',
+        length: 1087,
+        isbn: '9780765326362',
+        author: 'Brandon Sanderson',
+        series: 'The Stormlight Archive',
+    },
+    {
+        title: 'Oathbringer',
+        length: 1243,
+        isbn: '9780765326379',
+        author: 'Brandon Sanderson',
+        series: 'The Stormlight Archive',
+    },
+    {
+        title: 'Rhythm of War',
+        length: 1232,
+        isbn: '9780765326386',
+        author: 'Brandon Sanderson',
+        series: 'The Stormlight Archive',
+    },
+    {
+        title: 'The Final Empire',
+        length: 537,
+        isbn: '9780765311788',
+        author: 'Brandon Sanderson',
+        series: 'The Misborn Saga',
+    },
+    {
+        title: 'The Well of Ascension',
+        length: 578,
+        isbn: '9780765316882',
+        author: 'Brandon Sanderson',
+        series: 'The Misborn Saga',
+    },
+    {
+        title: 'The Hero of Ages',
+        length: 572,
+        isbn: '9780765316899',
+        author: 'Brandon Sanderson',
+        series: 'The Misborn Saga',
+    },
+    {
+        title: 'Warbreaker',
+        length: 688,
+        isbn: '9780575097469',
+        author: 'Brandon Sanderson',
+        series: 'Warbreaker',
+    },
+    
+    
+]
