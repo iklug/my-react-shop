da1ec3214c72572d1a40541ec303a3146d2c248e
Ian Klug <ianklug123@gmail.com>
2023-10-12 18:41:55 -0700

    finish header component, for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,24 @@
 const Header = () => {
     return (
      
-        <div className="flex flex-row bg-stone-50 shadow-md mb-12 fixed w-full">
-            <div className="h-24  flex">
-                <div>ICON</div>
+        <div className="h-20 flex flex-row bg-stone-50 shadow-md fixed w-full justify-between pl-6 pr-8">
+            <div className="h-16  flex">
+                <div className="h-16 w-16 mt-2 rounded-full bg-stone-50 border-4 border-stone-200 flex items-center justify-center pr-2 select-none">
+                    <p className="text-yellow-800 font-semibold">SAND</p>
+                    <p className="text-3xl text-yellow-400">||</p>
+                    <p className="text-yellow-800 font-semibold">SUN</p>
+                </div>
                 <div>
-                    <ul>
+                    <ul className="h-16 flex gap-6 pl-10 text-xl text-green-800 font-semibold items-end select-none">
                         <li>Home</li>
                         <li>Browse</li>
                     </ul>
                 </div>
             </div>
-            <div className="flex">
-            <ul>
-                <li>Wishlist</li>
-                <li>Cart</li>
+            <div >
+            <ul className="h-16 flex gap-4 text-xl justify-end items-end">
+                <li className="text-green-800 font-semibold select-none">Wishlist</li>
+                <li className="text-green-800 font-semibold select-none">Cart</li>
             </ul>
             </div>
             
